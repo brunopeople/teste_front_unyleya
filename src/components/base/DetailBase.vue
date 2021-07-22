cdcb4d52af299a518e3ca0019a49ebb3fbc14b38
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2021-07-22 13:03:25 -0300

    componente DetailResourceBase

---
+++ src/components/base/DetailBase.vue
@@ -0,0 +1,13 @@
+<template>
+    <DetailResourceBase :endpoint="$router.currentRoute.params.endpoint" />
+</template>
+
+<script>
+    import DetailResourceBase from "./DetailResourceBase";
+    export default {
+        name: "DetailBase",
+        components: {DetailResourceBase}
+    };
+</script>
+
+<style lang="scss" scoped></style>
