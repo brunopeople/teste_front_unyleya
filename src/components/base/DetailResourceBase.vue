b1f74090a43ad505ee6ef4336918108e31d82465
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2021-07-22 14:18:21 -0300

    componente DetailResourceBase

---
+++ src/components/base/DetailResourceBase.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <DetailAuthors v-if="endpoint == 'authors'" />
+        <DetailGenres v-else-if="endpoint == 'genres'" />
+        <DetailPublishers v-else-if="endpoint == 'publishers'" />
+        <DetailBooks v-else if="endpoint == 'books'" />
+        <h1 v-else>
+             Essa não :( <br/> {{endpoint || "Isso"}} não existir na API...
+        </h1>
+    </div>
+</template>
+
+<script>
+import DetailAuthors from "../../views/authors/DetailAuthors";
+import DetailGenres from "../../views/genres/DetailGenres";
+import DetailBooks from "../../views/books/DetailBooks";
+import DetailPublishers from "../../views/publishers/DetailPublishers";
+
+export defaul {
+    name: "DetailResourceBase",
+    props: ["endpoint"],
+    components:{
+        DetailAuthors,
+        DetailGenres,
+        DetailPublishers,
+        DetailBooks
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
