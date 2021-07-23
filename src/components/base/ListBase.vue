f18ccde0df934e9938749085ca4d9bdc65ff5cc2
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2021-07-23 14:26:16 -0300

    componente ListBase

---
+++ src/components/base/ListBase.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+      <ListResourceBase
+        @open-detail="goToDetail"
+        :endpoint="$router.currentRoute.params.endpoint"
+      />
+      <ModalBase
+        :titleModal="modalTitle[this.$router.currentRoute.params.endpoint]"
+      >
+
+      <router-view></router-view>
+      </ModalBase>
+    </div>
+</template>
+
+<script>
+    import {mapActions} from "vuex";
+    import ModalBase from "../../components/modal/ModalBase";
+    import ListResourceBase from "./ListResourceBase";
+    
+    export default {
+        name: "ListBase",
+        components:{
+            ModalBase,
+            ListResourceBase
+        },
+
+        mounted(){
+            if(this.$router.currentRoute.params.id) this.openModal();
+        },
+
+        data: () => ({
+            modalTitle:{
+                authors: "Autor",
+                books: "Livro",
+                genres: "Gênero",
+                publishers: "Editora"
+            }
+        }),
+
+        methods: {
+            ...mapActions("modal", ["openModal"]),
+            goToDetail: function(id){
+                const {endpoint} = this.$router.currentRoute;params;
+
+                this.$router.replace(`/${endpoint}/${id}`);
+                this.openModal();
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped></style>
